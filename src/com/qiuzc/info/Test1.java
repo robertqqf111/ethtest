9f50ecc12812fa0dd77e20cae6dc1020efeb17e8
robertqqf111 <14117806@qq.com>
2018-01-19 15:08:12 +0800

    项目提交测试

---
+++ src/com/qiuzc/info/Test1.java
@@ -0,0 +1,16 @@
+package com.qiuzc.info;
+
+//http://blog.csdn.net/u014079773/article/details/51595127
+
+public class Test1 {
+
+	public Test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/qiuzc/info/Test2.java
@@ -0,0 +1,14 @@
+package com.qiuzc.info;
+
+public class Test2 {
+
+	public Test2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
